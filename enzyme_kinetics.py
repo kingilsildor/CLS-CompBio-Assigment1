41703bd9e0d885bea0ae18f9d1fe66ee51a9d343
kingilsildor <tychostam@gmail.com>
2025-04-09 21:54:44 +0200

    calucalte constants

--- enzyme_kinetics.py
+++ enzyme_kinetics.py
@@ -50,11 +50,9 @@ def compare_output(tuple_output, titles):
 
     seq_output, ping_output = tuple_output
     if seq_output[0] > ping_output[0] and seq_output[1] < ping_output[1]:
-        # print("Sequential model is better.")
-        return "seq", seq_output
+        return "seq"
     elif ping_output[0] > seq_output[0] and ping_output[1] < seq_output[1]:
-        # print("Ping Pong model is better.")
-        return "ping", ping_output
+        return "ping"
     else:
         return "Both models are equally good or bad."
 
@@ -88,12 +86,16 @@ def plot_eadie_hofstee(df, save=False):
         x = df_substrate["Rate"] / df_substrate["S1"]
         y = df_substrate["Rate"]
 
-        slope, intercept = np.polyfit(x, y, 1)
-        constants[s2] = (slope, intercept)
+        K_m1, v_max = np.polyfit(x, y, 1)
+        constants[s2] = (K_m1, v_max)
 
         plt.scatter(x, y, label=f"[S2] = {s2} mM")
-        plt.plot(x, slope * x + intercept, "--")
-    plt.plot([], [], "--", label="$-K_m$", color="black")
+        plt.plot(
+            x,
+            K_m1 * x + v_max,
+            "--",
+            label=f"$K_{{m1}} = {K_m1:.2f}$ mM/s, $v_{{max}} = {v_max:.2f}$ mM/s",
+        )
 
     plt.xlabel(r"v / [S1] (mH$^{{-1}}s^{{-1}}$)")
     plt.ylabel(r"v (mM/s)")
@@ -107,6 +109,13 @@ def plot_eadie_hofstee(df, save=False):
     else:
         plt.show()
 
+    constants = pd.DataFrame.from_dict(
+        constants, orient="index", columns=["K_m1", "V_max"]
+    )
+    constants.index.name = "S2"
+    constants.reset_index(inplace=True)
+    return constants
+
 
 def plot_lineweaver_burk(df, save=False):
     plt.figure(figsize=(10, 6))
@@ -115,7 +124,6 @@ def plot_lineweaver_burk(df, save=False):
         inv_S1 = 1 / group["S1"]
         inv_Rate = 1 / group["Rate"]
         plt.plot(inv_S1, inv_Rate, "o-", label=f"[S2] = {s2} mM")
-    # plt.axvline(x=1 / v_max, color="black", linestyle="--")
 
     plt.xlabel(r"1/[S1] (mM$^{{-1}})$")
     plt.ylabel(r"1/v (s/mM)")
@@ -129,9 +137,10 @@ def plot_lineweaver_burk(df, save=False):
         plt.show()
 
 
-def calculate_constants():
-    ...
-    # K2 = (vmax * S1 * S2 - v * K1 * S2 - v * S1 * S2) / (v * S1)
+def calculate_constants(S2, v, v_max, K_m1):
+    S1 = 1.0
+    K_m2 = (v_max * S1 * S2 - v * K_m1 * S2 - v * S1 * S2) / (v * S1)
+    return K_m2
 
 
 def main():
@@ -139,9 +148,7 @@ def main():
 
     seq_output = fit_sequential_bisubstrate(data, sequential_model)
     ping_output = fit_sequential_bisubstrate(data, ping_pong_model)
-    model_str, model_output = compare_output(
-        (seq_output, ping_output), ("Sequential", "Ping Pong")
-    )
+    model_str = compare_output((seq_output, ping_output), ("Sequential", "Ping Pong"))
 
     s2_range = [1.5, 2.5, 5.0]
     sim_amount = 100
@@ -159,8 +166,20 @@ def main():
 
     df_simulated_data.to_csv("data/simulated_data.csv", index=False)
 
-    plot_eadie_hofstee(df_simulated_data, save=True)
+    constants = plot_eadie_hofstee(df_simulated_data, save=True)
     plot_lineweaver_burk(df_simulated_data, save=True)
+
+    print(df_simulated_data.head())
+
+    for S2 in constants["S2"]:
+        v_max = constants[constants["S2"] == S2]["V_max"].values[0]
+        K_m1 = constants[constants["S2"] == S2]["K_m1"].values[0]
+        K_m2 = calculate_constants(S2, v, v_max, K_m1)
+        print(f"S2: {S2} mM")
+        print(f"v: {v:.4f} mM/s")
+        print(f"v_max: {v_max:.4f} mM/s")
+        print(f"K_m1: {K_m1:.4f} mM")
+        print(f"K_m2: {K_m2:.4f} mM\n")
 
 
 if __name__ == "__main__":
