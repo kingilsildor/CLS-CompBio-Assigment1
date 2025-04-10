06700694eb45732cb377d3c638cc82d661820c8f
kingilsildor <tychostam@gmail.com>
2025-04-10 10:42:02 +0200

    added documentation

--- enzyme_kinetics.py
+++ enzyme_kinetics.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from scipy.optimize import curve_fit
 
+######## Setup Plotting Sytle ############
 FIG_DPI = 400
 FIG_SIZE = (10, 6)
 
@@ -11,42 +12,124 @@ plt.rcParams["axes.labelsize"] = 14
 plt.rcParams["xtick.labelsize"] = 12
 plt.rcParams["ytick.labelsize"] = 12
 plt.rcParams["legend.fontsize"] = 12
+##########################################
 
 
-def r_squared(y_true, y_pred):
+def r_squared(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Calculate the R-squared value for the given true and predicted values.
+
+    Params
+    ------
+    - y_true (np.ndarray): True values.
+    - y_pred (np.ndarray): Predicted values.
+
+    Returns
+    -------
+    - float: R-squared value.
+    """
+    assert len(y_true) == len(y_pred), "y_true and y_pred must have the same length."
     ss_res = np.sum((y_true - y_pred) ** 2)
     ss_tot = np.sum((y_true - np.mean(y_true)) ** 2)
     return 1 - (ss_res / ss_tot)
 
 
-def chi_squared(y_true, y_pred):
+def chi_squared(y_true: np.ndarray, y_pred: np.ndarray) -> float:
+    """
+    Calculate the chi-squared value for the given true and predicted values.
+
+    Params
+    ------
+    - y_true (np.ndarray): True values.
+    - y_pred (np.ndarray): Predicted values.
+
+    Returns
+    -------
+    - float: Chi-squared value.
+    """
+    assert len(y_true) == len(y_pred), "y_true and y_pred must have the same length."
     return np.sum(((y_true - y_pred) ** 2) / y_true)
 
 
-def sequential_model(S, V_max, K_m1, K_m2):
+def sequential_model(S: tuple, V_max: float, K_m1: float, K_m2: float) -> float:
+    """
+    Sequential bisubstrate model for enzyme kinetics.
+
+    Params
+    ------
+    - S (tuple): Substrate concentrations (S1, S2).
+    - V_max (float): Maximum reaction rate.
+    - K_m1 (float): Michaelis constant for substrate 1.
+    - K_m2 (float): Michaelis constant for substrate 2.
+
+    Returns
+    -------
+    - float: Reaction rate.
+    """
     S1, S2 = S
     v = (V_max * S1 * S2) / (K_m1 * K_m2 + K_m2 * S1 + K_m1 * S2 + S1 * S2)
     return v
 
 
-def ping_pong_model(S, V_max, K_m1, K_m2):
+def ping_pong_model(S: tuple, V_max: float, K_m1: float, K_m2: float) -> float:
+    """
+    Ping Pong bisubstrate model for enzyme kinetics.
+
+    Params
+    ------
+    - S (tuple): Substrate concentrations (S1, S2).
+    - V_max (float): Maximum reaction rate.
+    - K_m1 (float): Michaelis constant for substrate 1.
+    - K_m2 (float): Michaelis constant for substrate 2.
+
+    Returns
+    -------
+    - float: Reaction rate.
+    """
     S1, S2 = S
     v = (V_max * S1 * S2) / (K_m1 * S2 + K_m2 * S1 + S1 * S2)
     return v
 
 
 def calculate_k2(
-    S1,
-    S2,
-    v,
-    V_max,
-    K_m1,
-):
+    S1: float,
+    S2: float,
+    v: float,
+    V_max: float,
+    K_m1: float,
+) -> float:
+    """
+    Calculate K_m2 using the ping-pong model.
+
+    Params
+    ------
+    - S1 (float): Concentration of substrate 1.
+    - S2 (float): Concentration of substrate 2.
+    - v (float): Reaction rate.
+    - V_max (float): Maximum reaction rate.
+    - K_m1 (float): Michaelis constant for substrate 1.
+
+    Returns
+    -------
+    - float: K_m2 value.
+    """
     K_m2 = (V_max * S1 * S2 - v * K_m1 * S2 - v * S1 * S2) / (v * S1)
     return K_m2
 
 
-def fit_sequential_bisubstrate(data, model):
+def fit_sequential_bisubstrate(data: pd.DataFrame, model: callable) -> tuple:
+    """
+    Fit the sequential bisubstrate model to the data.
+
+    Params
+    ------
+    - data (pd.DataFrame): Data containing substrate concentrations and rates.
+    - model (function): The model function to fit.
+
+    Returns
+    -------
+    - tuple: R-squared value, chi-squared value, and fitted parameters.
+    """
     S = data[["S1", "S2"]].values.T
     rates = data["Rate"].values
 
@@ -59,12 +142,33 @@ def fit_sequential_bisubstrate(data, model):
     return (r2_score, chi2_score, params)
 
 
-def compare_output(tuple_output, titles):
-    for output, title in zip(tuple_output, titles):
-        print(f"{title} Model Results:")
-        print(f"R-squared: {output[0]:.4f}")
-        print(f"Chi-squared: {output[1]:.4f}")
-        print(f"Parameters: {output[2]}\n")
+def compare_output(tuple_output: tuple, titles: tuple) -> str:
+    """
+    Compare the outputs of two models and print the results.
+    Save the results to a file and return the model with the better fit.
+
+    Params
+    ------
+    - tuple_output (tuple): Tuple containing the outputs of the two models.
+    - titles (tuple): Tuple containing the titles of the two models.
+
+    Returns
+    -------
+    - str: The model with the better fit.
+    """
+    assert len(tuple_output) == 2, "tuple_output must contain two elements."
+
+    with open("data/model_results.txt", "w") as file:
+        for output, title in zip(tuple_output, titles):
+            file.write(f"{title} Model Results:\n")
+            file.write(f"R-squared: {output[0]:.4f}\n")
+            file.write(f"Chi-squared: {output[1]:.4f}\n")
+            file.write(f"Parameters: {output[2]}\n\n")
+
+            print(f"{title} Model Results:")
+            print(f"R-squared: {output[0]:.4f}")
+            print(f"Chi-squared: {output[1]:.4f}")
+            print(f"Parameters: {output[2]}\n")
 
     seq_output, ping_output = tuple_output
     if seq_output[0] > ping_output[0] and seq_output[1] < ping_output[1]:
@@ -76,12 +180,27 @@ def compare_output(tuple_output, titles):
 
 
 def simulate_data(
-    model,
-    data,
-    s1_original,
-    s2_values,
-    simulate_amount=100,
+    model: callable,
+    data: pd.DataFrame,
+    s2_values: np.ndarray,
+    s1_original: bool = True,
+    simulate_amount: int = 100,
 ):
+    """
+    Simulate data using the fitted model.
+
+    Params
+    ------
+    - model (function): The model function to use for simulation.
+    - data (pd.DataFrame): Data containing substrate concentrations and rates.
+    - s2_values (np.ndarray): List of S2 values to simulate.
+    - s1_original (bool): Whether to use the original S1 values or a range. Default is True.
+    - simulate_amount (int): Number of points to simulate. Default is 100.
+
+    Returns
+    -------
+    - pd.DataFrame: Simulated data.
+    """
     params, _ = curve_fit(model, (data["S1"], data["S2"]), data["Rate"])
 
     if s1_original:
@@ -105,9 +224,16 @@ def simulate_data(
     return simulated_data
 
 
-def plot_eadie_hofstee(df, save=False):
-    plt.figure(figsize=FIG_SIZE)
+def plot_eadie_hofstee(df: pd.DataFrame, save=False) -> None:
+    """
+    Plot the Eadie-Hofstee plot for the given data.
 
+    Params
+    ------
+    - df (pd.DataFrame): Data containing substrate concentrations and rates.
+    - save (bool): Whether to save the plot. Default is False.
+    """
+    plt.figure(figsize=FIG_SIZE)
     km2_results = []
 
     s2_values = df["S2"].unique()
@@ -162,7 +288,15 @@ def plot_eadie_hofstee(df, save=False):
         plt.show()
 
 
-def plot_lineweaver_burk(df, save=False):
+def plot_lineweaver_burk(df: pd.DataFrame, save=False) -> None:
+    """
+    Plot the Lineweaver-Burk plot for the given data.
+
+    Params
+    ------
+    - df (pd.DataFrame): Data containing substrate concentrations and rates.
+    - save (bool): Whether to save the plot. Default is False.
+    """
     plt.figure(figsize=FIG_SIZE)
 
     for s2, group in df.groupby("S2"):
